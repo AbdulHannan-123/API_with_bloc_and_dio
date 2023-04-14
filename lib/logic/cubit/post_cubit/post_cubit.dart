5905b450ae4e5bfb55047339d22209875fba18c6
AbdulHannan-123 <ah6475774@gmail.com>
2023-04-15 00:09:46 +0500

    showing the state of bloc

--- lib/logic/cubit/post_cubit/post_cubit.dart
+++ lib/logic/cubit/post_cubit/post_cubit.dart
@@ -1,10 +1,13 @@
 import 'package:calling_api_with_bloc_and_dio/data/models/post_model.dart';
 import 'package:calling_api_with_bloc_and_dio/data/repositories/post_repository.dart';
 import 'package:calling_api_with_bloc_and_dio/logic/cubit/post_cubit/post_state.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class PostCubit extends Cubit<PostState> {
-  PostCubit() :super(PostLoadingState());
+  PostCubit() :super(PostLoadingState()){
+    fetchpost();
+  }
 
   PostRepositories postRepositories = PostRepositories();
 
@@ -12,8 +15,10 @@ class PostCubit extends Cubit<PostState> {
     try {
       List<PostModel> posts =  await postRepositories.fetchpost();
       emit(PostLoadedState(posts));
-    } catch (e) {
-      emit(PostErrorState(e.toString()));
+    } on DioError catch (e) {
+      if (e.type == DioErrorType.unknown) {
+        emit(PostErrorState("the error is unknown"));
+      }
     }
   }
   
--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,6 +1,8 @@
+import 'package:calling_api_with_bloc_and_dio/data/models/post_model.dart';
+import 'package:calling_api_with_bloc_and_dio/logic/cubit/post_cubit/post_cubit.dart';
+import 'package:calling_api_with_bloc_and_dio/logic/cubit/post_cubit/post_state.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -17,7 +19,36 @@ class _HomeScreenState extends State<HomeScreen> {
         title: const Text("Api Handling"),
       ),
       body: SafeArea(
-        child: ListView(),
+        child: BlocConsumer<PostCubit, PostState>(
+          listener: (context, state) {
+            if (state is PostErrorState) {
+              SnackBar snackBar = SnackBar(content: Text(state.error), backgroundColor: Colors.red,);
+              ScaffoldMessenger.of(context).showSnackBar(snackBar);
+            }
+          },
+          builder: (context, state) {
+           if (state is PostLoadingState) {
+             return const Center(child:  CircularProgressIndicator());
+           }
+
+           if( state is PostLoadedState){
+            return ListView.builder(
+              itemCount: state.posts.length,
+              itemBuilder: (context, index) {
+                PostModel post = state.posts[index];
+                return ListTile(
+                  title: Text(post.title.toString()),
+                  subtitle: Text(post.completed.toString()),
+                );
+              },
+              );
+           }
+           
+           return const Center(
+            child:  Text(" an error occour"),
+           );
+          },
+        ),
       ),
     );
   }
